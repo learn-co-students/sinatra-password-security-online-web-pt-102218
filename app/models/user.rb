9c489e6d7e29c662809745bb93d022466001c713
grant smith <grantsmith1256@gmail.com>
2019-02-17 17:43:37 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
-	
+	class CreateUsers < ActiveRecord::Migration[5.1]
+
+		def up
+    	create_table :users do |t|
+      	t.string :username
+      	t.string :password_digest
+    	end
+  	end
+
+  	def down
+    	drop_table :users
+  	end
+	end
 end
